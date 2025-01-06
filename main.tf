3e9345e09050875eb53624642b2a0f76946016e4
Gillian Stravers <gillianstravers@icloud.com>
2025-01-06 15:35:47 +0100

    typo

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "azurerm_vpn_gateway_connection" "this" {
 
     content {
       name                                  = vpn_link.value.name
-      vpn_site_link_id                      = azurerm_vpn_site.this[each.value.remote_vpn_site_name].link[vpn_link.value.name].id      bandwidth_mbps                        = try(vpn_link.value.bandwidth_mbps, null)
+      vpn_site_link_id                      = lookup(azurerm_vpn_site.this, each.value.remote_vpn_site_name).id
       bgp_enabled                           = try(vpn_link.value.bgp_enabled, null)
       connection_mode                       = try(vpn_link.value.connection_mode, null)
       protocol                              = try(vpn_link.value.protocol, null)

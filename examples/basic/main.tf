d17e515cbd8ff91831e9e6f6fc6974cd7cc0c838
Gillian Stravers <gillianstravers@icloud.com>
2025-01-13 14:50:19 +0100

    formatting

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -10,34 +10,34 @@ resource "azurerm_resource_group" "this" {
 }
 
 resource "azurerm_vpn_site" "this" {
-  for_each            = {
+  for_each = {
     site1 = {
-      name = "example-vpn-site1"
+      name          = "example-vpn-site1"
       address_cidrs = ["10.0.0.0/24"]
       links = [
         {
-          name = "link1"
-          ip_address = "192.168.1.1"
+          name          = "link1"
+          ip_address    = "192.168.1.1"
           provider_name = "Provider1"
           speed_in_mbps = 100
           bgp_settings = {
-            asn = 65001
+            asn                 = 65001
             bgp_peering_address = "192.168.1.2"
           }
         }
       ]
     }
     site2 = {
-      name = "example-vpn-site2"
+      name          = "example-vpn-site2"
       address_cidrs = ["10.1.0.0/24"]
       links = [
         {
-          name = "link2"
-          ip_address = "192.168.2.1"
+          name          = "link2"
+          ip_address    = "192.168.2.1"
           provider_name = "Provider2"
           speed_in_mbps = 200
           bgp_settings = {
-            asn = 65002
+            asn                 = 65002
             bgp_peering_address = "192.168.2.2"
           }
         }
@@ -78,33 +78,33 @@ resource "azurerm_vpn_site" "this" {
 resource "azurerm_vpn_gateway_connection" "this" {
   for_each = {
     connection1 = {
-      vpn_gateway_name = "example-vpn-gateway1"
-      vpn_site_name = "example-vpn-site1"
+      vpn_gateway_name          = "example-vpn-gateway1"
+      vpn_site_name             = "example-vpn-site1"
       internet_security_enabled = true
       vpn_links = [
         {
-          name = "link1"
-          bandwidth_mbps = 100
-          bgp_enabled = true
-          connection_mode = "Default"
-          protocol = "IKEv2"
-          ratelimit_enabled = false
-          route_weight = 10
-          shared_key = "example-shared-key"
-          local_azure_ip_address_enabled = true
+          name                                  = "link1"
+          bandwidth_mbps                        = 100
+          bgp_enabled                           = true
+          connection_mode                       = "Default"
+          protocol                              = "IKEv2"
+          ratelimit_enabled                     = false
+          route_weight                          = 10
+          shared_key                            = "example-shared-key"
+          local_azure_ip_address_enabled        = true
           policy_based_traffic_selector_enabled = false
           ipsec_policy = {
-            dh_group = "DHGroup14"
+            dh_group                 = "DHGroup14"
             ike_encryption_algorithm = "AES256"
-            ike_integrity_algorithm = "SHA256"
-            encryption_algorithm = "AES256"
-            integrity_algorithm = "SHA256"
-            pfs_group = "PFS2"
-            sa_data_size_kb = 102400000
-            sa_lifetime_sec = 3600
+            ike_integrity_algorithm  = "SHA256"
+            encryption_algorithm     = "AES256"
+            integrity_algorithm      = "SHA256"
+            pfs_group                = "PFS2"
+            sa_data_size_kb          = 102400000
+            sa_lifetime_sec          = 3600
           }
           custom_bgp_address = {
-            ip_address = "192.168.1.3"
+            ip_address          = "192.168.1.3"
             ip_configuration_id = "/subscriptions/your-subscription-id/resourceGroups/example-resource-group/providers/Microsoft.Network/virtualNetworkGateways/example-vpn-gateway1/ipConfigurations/ipconfig1"
           }
         }
